59228937344bc924dae2a6efc578b5867a368bd8
amirnsamimi <amirnsamimi@gmail.com>
2024-06-26 16:28:56 +0330

    styles defined on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,58 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
     extend: {
+      blur:{
+        '4': '0.125rem',
+        '8': '0.25rem',
+        '12': '0.375rem',
+        '16': '0.5rem',
+        '20': '0.625rem',
+        '24': '0.75rem',
+        '28': '0.875rem',
+        '32': '1rem' 
+      },
+      boxShadow:{
+        'mshadow-1':'0px 1px 3px 1px #0000004D,  0px 1px 3px 1px #00000026',
+        'mshadow-2': '0px 1px 3px 0px #0000004D,box-shadow: 0px 4px 8px 3px #00000026',
+        'mshadow-3': '0px 1px 3px 0px #0000004D,box-shadow: 0px 4px 8px 3px #00000026',
+        'mshadow-4':'0px 4px 4px 0px #0000004D,  0px 8px 12px 6px #00000026',
+        'mshadow-5': '0px 1px 3px 0px #0000004D,box-shadow: 0px 4px 8px 3px #00000026',
+        'cshadow-1': '0px 1px 1px 0px #0000004A,0px 2px 2px 0px #00000042,0px 5px 3px 0px #00000026,0px 9px 4px 0px #0000000A,0px 14px 4px 0px #00000003',
+        'cshadow-2': '0px 1px 2px 0px #00000033,0px 5px 5px 0px #0000002B,0px 10px 6px 0px #0000001A,0px 18px 7px 0px #00000008,0px 28px 8px 0px #00000000',
+        'cshadow-3': '0px 2px 4px 0px #00000040,0px 7px 7px 0px #00000036,0px 15px 9px 0px #00000021,0px 27px 11px 0px #0000000A,0px 43px 12px 0px #00000000',
+        'cshadow-4': '0px 2px 4px 0px #00000063,0px 8px 8px 0px #00000057,0px 18px 11px 0px #00000033,0px 32px 13px 0px #0000000F,0px 50px 14px 0px #00000003',
+        'cshadow-5': '0px 3px 7px 0px #00000063,0px 12px 12px 0px #00000057,0px 27px 16px 0px #00000033,0px 48px 19px 0px #0000000F,0px 75px 21px 0px #00000003',
+      },
+      borderRadius:{
+        '4': '0.25rem',
+        '8': '0.5rem',
+        '12': '0.75rem',
+        '16': '1rem',
+        '20': '1.25rem',
+        '24': '1.5rem',
+        '28': '1.75rem',
+        '32': '2rem'        
+      },
+      fontSize:{
+        'h1': '4.25rem',
+        'h2': '4rem',
+        'h3': '3.75rem',
+        'h4': '3.5rem',
+        'h5': '3.25rem',
+        'h6': '3rem',
+        'h7': '2.75rem',
+        'h8': '2.5rem',
+        'highlights': '2.25rem',
+        'body-1': '2rem',
+        'body-2': '1.75rem',
+        'subtitle-1': '1.5rem',
+        'subtitle-2': '1.25rem',
+        'button-1': '1rem',
+        'button-2': '0.875rem',
+        'mini-card': '0.75rem'
+      },
      colors:{
       transparent: 'transparent',
       current: 'currentColor',
@@ -102,3 +153,6 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+
+
+/* Rectangle 1125 */
